64f4af93da25844a79fa1d789b92ae5436eb3375
Stuart <kd5pbo@gmail.com>
2014-08-29 03:04:38 -0400

    Can now handle non-ipv4-addresses

--- ipsorter.go
+++ ipsorter.go
@@ -26,43 +26,99 @@ func (a byIP) Less(i, j int) bool {
 		n, _ := strconv.Atoi(t[k])
 		/* Try the next if they're equal */
 		if m == n {
-                        continue
+			continue
 		} else {
-                        return m < n
-                }
+			return m < n
+		}
 	}
 	return false
 }
 
-//Sort4 returns a sorted slice of IPv4 addresses with optional port numbers.
-//Strings which are not IPv4 addressses are discarded.
-func Sort4(a []string) []string {
+////Sort4 returns a sorted slice of IPv4 addresses with optional port numbers.
+////Strings which are not IPv4 addressses are discarded.
+//func Sort4(a []string) []string {
+//	/* Make the regex the first time it's used */
+//	if nil == re {
+//		re = regexp.MustCompile(res)
+//	}
+//	/* Slice to hold results */
+//	r := []string{}
+//	/* Iterate over input slice to make output slice */
+//CheckLoop:
+//	for _, s := range a {
+//		/* Split into octets */
+//		o := re.FindStringSubmatch(s)
+//		/* Give up if it didn't match at all */
+//		if len(o) < 5 {
+//			continue
+//		}
+//		/* Make sure each match is in the right range */
+//		for i := 1; i < 5; i++ {
+//			n, err := strconv.Atoi(o[i])
+//			if err != nil || n < 0 || n > 255 {
+//				continue CheckLoop
+//			}
+//		}
+//		/* Add to the slice to be sorted */
+//		r = append(r, s)
+//	}
+//	/* Sort the output slice */
+//	sort.Sort(byIP(r))
+//	return r
+//}
+
+//Sort4 returns a sorted slice of IPv4 addresses, discarding other strings.
+func Sort4(unsorted []string) []string {
+        s, _ := Sort(unsorted, false)
+        return s
+}
+
+//Sort returns a sorted slice of IPv4 addresses with optional port numbers.
+//Elements in the list to be sorted that are not IPv4 adresses will be
+//lexicographically sorted and returned as the second return value if rem is
+//true.
+func Sort(unsorted []string, rem bool) (sorted, nonaddr []string) {
+	if rem {
+		nonaddr = []string{}
+	}
 	/* Make the regex the first time it's used */
 	if nil == re {
 		re = regexp.MustCompile(res)
 	}
 	/* Slice to hold results */
-        r := []string{}
+	sorted = []string{}
 	/* Iterate over input slice to make output slice */
 CheckLoop:
-	for _, s := range a {
+	for _, s := range unsorted {
 		/* Split into octets */
 		o := re.FindStringSubmatch(s)
 		/* Give up if it didn't match at all */
 		if len(o) < 5 {
+			/* Add it to the list of unsorted items */
+			if rem {
+				nonaddr = append(nonaddr, s)
+			}
 			continue
 		}
 		/* Make sure each match is in the right range */
 		for i := 1; i < 5; i++ {
 			n, err := strconv.Atoi(o[i])
 			if err != nil || n < 0 || n > 255 {
+				/* Add it to the list of unsorted items */
+				if rem {
+					nonaddr = append(nonaddr, s)
+				}
 				continue CheckLoop
 			}
 		}
 		/* Add to the slice to be sorted */
-		r = append(r, s)
+		sorted = append(sorted, s)
 	}
 	/* Sort the output slice */
-	sort.Sort(byIP(r))
-	return r
+	sort.Sort(byIP(sorted))
+        /* Sort the non-address slice, if we have it */
+        if rem {
+                sort.Sort(sort.StringSlice(nonaddr))
+        }
+	return sorted, nonaddr
 }
